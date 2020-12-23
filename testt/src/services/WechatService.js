e66e40dfd0a0d925af870ec50945ef10b534c317
sean <nan.a.shen@accenture.com>
2020-12-23 16:00:47 +0800

    add qr

---
+++ testt/src/services/WechatService.js
@@ -0,0 +1,43 @@
+import req from './index'
+import store from '../store'
+
+export default {
+  async getUserById () {
+
+    const response = await req.request.get('/user/:id')
+    return response.data
+  },
+  async register (data) {
+    return await req.request.post(
+      '/user/',
+      data
+    )
+  },
+  async getQRCode (id) {
+    var data = {"expire_seconds": 604800, "action_name": "QR_STR_SCENE", "action_info": {"scene": {"scene_str": id}}}
+    alert("kaishi")
+    var ticket =await req.wapirequest.post(`/qrcode/create?access_token=${store.state.user.wechat}`,data,function (error, response, body) {
+      if(error!==null){
+        alert(error)
+       // reject("获取access_token失败 检查getAccessToken函数");
+      }
+      alert(JSON.parse(body));
+    });        
+    alert("jieshu")
+      if (ticket) {
+      
+        return  'https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket='+ticket.data.ticket
+      }else{
+        return ''
+      }
+     
+},
+  async login (data) {
+
+    const response = await req.request.post(
+      '/user/login',
+      data
+    )
+    return response
+  }
+}
