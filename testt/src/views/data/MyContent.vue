57b0619a78e2b9e19f6b3d6ec61e4c79601e7f5d
xintong.zou <xintong.zou@accenture.com>
2021-01-14 19:54:18 +0800

    fix bug: 1.MySE,MyPush,MyMeeting页面自带横向滚动条 2.当触发了结束时分的未填写事件，此时选择开始时分，结束时分默认赋值后，提示取消

--- testt/src/views/data/MyContent.vue
+++ testt/src/views/data/MyContent.vue
@@ -428,7 +428,7 @@ export default {
             //var content = ContentService.downloadFile({ContentID:data.ContentID,filePath:file.FileName})  
             this.fileList.push({"name":file.FileName,"url":'/api/myContent/downloadpdf?ContentID='+data.ContentID+'&file='+file.FileName})
           });
-          console.log(this.fileList)
+          //console.log(this.fileList)
         })
         .catch(function (err) {
           console.log(err);
@@ -755,20 +755,28 @@ export default {
       })
     },
     dateFormat(time) {
-      var date=new Date(time);
-      var year=date.getFullYear();
-      /* 在日期格式中，月份是从0开始的，因此要加0
-        * 使用三元表达式在小于10的前面加0，以达到格式统一  如 09:11:05
-        * */
-      var month= date.getMonth()+1<10 ? "0"+(date.getMonth()+1) : date.getMonth()+1;
-      var day=date.getDate()<10 ? "0"+date.getDate() : date.getDate();
-      var hours=date.getHours()<10 ? "0"+date.getHours() : date.getHours()-8;
-      var minutes=date.getMinutes()<10 ? "0"+date.getMinutes() : date.getMinutes();
-      var seconds=date.getSeconds()<10 ? "0"+date.getSeconds() : date.getSeconds();
-      // 拼接
-      //return year+"-"+month+"-"+day+" "+hours+":"+minutes;
-      return year+"-"+month+"-"+day+" "+hours+":"+minutes+":"+seconds
-  },
+      if(time == null)
+      {
+        return "";
+      }
+      else{
+
+        var date=new Date(time);
+        var year=date.getFullYear();
+        
+        /* 在日期格式中，月份是从0开始的，因此要加0
+          * 使用三元表达式在小于10的前面加0，以达到格式统一  如 09:11:05
+          * */
+        var month= date.getMonth()+1<10 ? "0"+(date.getMonth()+1) : date.getMonth()+1;
+        var day=date.getDate()<10 ? "0"+date.getDate() : date.getDate();
+        var hours=date.getHours()<10 ? "0"+date.getHours() : date.getHours();
+        var minutes=date.getMinutes()<10 ? "0"+date.getMinutes() : date.getMinutes();
+        var seconds=date.getSeconds()<10 ? "0"+date.getSeconds() : date.getSeconds();
+        // 拼接
+        //return year+"-"+month+"-"+day+" "+hours+":"+minutes;
+        return year+"-"+month+"-"+day+" "+hours+":"+minutes+":"+seconds
+      }
+    },
 
   },
   computed: {
@@ -820,7 +828,7 @@ export default {
 .title{
   width: 15%;
   color: white;
-  padding-left:5px;
+  //padding-left:5px;
   }
 
 .Button{
